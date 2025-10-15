416bc6940f1f9a4805681c983a3e03226c5db0d1
Henrique Forin <henriquerudzevicius@gmail.com>
2025-10-15 14:18:29 -0300

    Primeiro commit

--- app/Http/Controllers/User/UserViewController.js
+++ app/Http/Controllers/User/UserViewController.js
@@ -1,10 +1,16 @@
-
+import UserModel from "../../../Models/UserModel.js"; // ajuste o caminho se necessário
 
 export default async function UserViewController(request, response) {
+    // Usuário atual vindo do middleware/token
+    const currentUser = request.user;
+
+    // Buscar todos os usuários pelo model
+    const users = await UserModel.findAll();
 
     return response.render("users", {
         header: "Aula 07 - V de Views, SSR e SSG",
-        user: request.user
+        user: currentUser,
+        users: users
     });
-
+    
 };
